70e543b775957b747e5ce06dd994ee31bbec4c6e
clarkwill-dev <willsclark@gmail.com>
2026-07-07 15:42:45 -0700

    light dotfiles

---
+++ configuration.nix
@@ -0,0 +1,50 @@
+{...}:
+
+{
+# Determinate already manages the Nix daemon, so nix-daemon shouldn't
+nix.enable = false;
+
+nixpkgs.config.allowUnfree = true;
+nixpkgs.hostPlatform = "aarch64-darwin";
+
+system.primaryUser = "willclark";
+users.users.willclark = {
+   home = "/Users/willclark";
+};
+system.stateVersion = 6;
+
+system.defaults = {
+	NSGlobalDomain = {
+		AppleInterfaceStyle = null;
+		KeyRepeat = 2;
+		InitialKeyRepeat = 15;
+		_HIHideMenuBar = true;
+		AppleShowAllExtensions = true;
+};
+	dock.autohide = true;
+	finder.FXPreferredViewStyle = "Nlsv"; # list view by default
+	finder.CreateDesktop = false;
+	trackpad.Clicking = true;
+};
+	nix-homebrew = {
+	    enable = true;
+	    user = "willclark";
+	  };
+	  homebrew = {
+	    enable = true;
+	    onActivation.cleanup = "zap";  # remove anything not listed here
+	    onActivation.autoUpdate = true;
+	    onActivation.extraFlags = [ "--force" ];
+	    brews = [
+	      "herdr"
+	      "fx"
+	      "uv"
+	    ];
+	    casks = [
+	      "ghostty"
+	      "claude-code"
+	      "sioyek"
+	      "discord"
+	    ];
+	  };
+}
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager}: {
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./configuration.nix
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "willclark";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+	home-manager.darwinModules.home-manager
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.willclark = import ./home.nix;
+          }
+      ];
+    };
+  };
+}
